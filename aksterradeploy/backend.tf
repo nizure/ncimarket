f83484d1e6d900fb54de58308bc2d181dbfd6b99
Nizar El Ouarti <nizare@microsoft.com>
2021-04-07 22:11:17 +0100

    backend delete

--- aksterradeploy/backend.tf
+++
@@ -1,9 +0,0 @@
-# ## Modify the following to meet your backend. This example is for AzureRM
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = ""
-#     storage_account_name = ""
-#     container_name       = ""
-#     key                  = ""
-#   }
-# }
